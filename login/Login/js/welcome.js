bfb6c9c31a8ddd44dd1ac77a46c9c29ebe3bdfb0
zahrazakiyya <zzakiyyaapriliana@gmail.com>
2026-05-31 11:31:45 +0700

    Update welcome.js

--- login/Login/js/welcome.js
+++ login/Login/js/welcome.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     function goLogin() {
-        window.location.href = "login/index.html";
+        window.location.href = "login/Login/index.html";
     }
 
     function logout() {
